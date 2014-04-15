0078a4513cef9f08be04a85c9ba2da7f42208925
Edward <lihangqw@126.com>
2014-04-15 09:58:58 +0800

    Initial Commit

---
+++ layer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  layer
+//
+//  Created by LiHang on 14-4-15.
+//  Copyright (c) 2014年 LiHang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ layer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  layer
+//
+//  Created by LiHang on 14-4-15.
+//  Copyright (c) 2014年 LiHang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
